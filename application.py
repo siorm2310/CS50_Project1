0a774457a06d8f065bc30a1aed5aad915233ca8f
סיאור מאיר <siorm2310@gmail.com>
2019-10-09 11:11:21 +0300

    Added redirect from index to login

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template,redirect,url_for,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,11 +20,14 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/", methods=['POST','GET'] )
 def main():
-    return render_template("index.html")
-    
+    # TOOO: if logged, open index. if not open login
+    if 'connected_user' in session:
+        return render_template("index.html")
+    return redirect(url_for('login'))
+
+
 @app.route("/book/<string:isbn_num>", methods=['POST','GET'] )
 def book_page(isbn_num):
     book = db.execute(f"SELECT * FROM books WHERE isbn = '{isbn_num}'").fetchone()
@@ -34,9 +37,16 @@ def book_page(isbn_num):
     # TODO: get all book reviews
     return render_template("book_page.html", book = book)
 
+
 @app.route("/login", methods=["POST","GET"])
 def login():
+    # username = request.form.get('username')
+    # password = request.form.get('password')
+    if request.method == 'POST':
+        session['username'] = request.form['username'] # ???
+        return redirect(url_for('index')) # ???
     return render_template("login.html")
+
 
 @app.route("/register", methods=["POST","GET"])
 def register():
